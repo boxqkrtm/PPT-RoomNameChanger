d8639f632a8d7c4a967c4267aa0429d9239c6ad7
boxqkrtm <9902abcd@gmail.com>
2020-02-17 17:43:48 +0900

    version 2 add wIncount setting

--- PPT-RoomNameChanger/MainWindow.xaml.cs
+++ PPT-RoomNameChanger/MainWindow.xaml.cs
@@ -30,9 +30,72 @@ namespace PPT_RoomNameChanger
             InitializeComponent();
             worker = new Thread(() => Run(0));
             worker.Start();
+
+            a5.Click += radioButtons_CheckedChanged;
+            a10.Click += radioButtons_CheckedChanged;
+            a15.Click += radioButtons_CheckedChanged;
+            a20.Click += radioButtons_CheckedChanged;
+            a25.Click += radioButtons_CheckedChanged;
+            a30.Click += radioButtons_CheckedChanged;
+            a50.Click += radioButtons_CheckedChanged;
+            a100.Click += radioButtons_CheckedChanged;
         }
 
-        private void Run(int val)
+        public void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            //RadioButton radioButton = sender as RadioButton;
+            int ft = 1;
+            if (a5.IsChecked == true)
+            {
+                ft = 5;
+            }
+            else if (a10.IsChecked == true)
+            {
+                ft = 10;
+            }
+            else if (a15.IsChecked == true)
+            {
+                ft = 15;
+            }
+            else if (a20.IsChecked == true)
+            {
+                ft = 20;
+            }
+            else if (a25.IsChecked == true)
+            {
+                ft = 25;
+            }
+            else if (a30.IsChecked == true)
+            {
+                ft = 30;
+            }
+            else if (a50.IsChecked == true)
+            {
+                ft = 50;
+            }
+            else if (a100.IsChecked == true)
+            {
+                ft = 100;
+            }
+
+            Game = new ProcessMemory("puyopuyotetris");
+            Game.WriteInt32(new IntPtr(
+            Game.ReadInt32(new IntPtr(
+                Game.ReadInt32(new IntPtr(
+                     Game.ReadInt32(new IntPtr(
+                            Game.ReadInt32(new IntPtr(
+                                Game.ReadInt32(new IntPtr(
+                                    0x140460690
+                                )) + 0x20
+                            )) + 0x20
+                        )) + 0x20
+                    )) + 0x610
+                )) + 0x4), ft);
+        }
+
+
+
+           private void Run(int val)
         {
             while (true)
             {
@@ -86,7 +149,7 @@ namespace PPT_RoomNameChanger
                     )) + 0x610
                 )) + 0x4
                 + 0xAC),
-                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
+                "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
             Game.WriteStringUnicode(new IntPtr(
             Game.ReadInt32(new IntPtr(
                 Game.ReadInt32(new IntPtr(
@@ -102,6 +165,16 @@ namespace PPT_RoomNameChanger
                 + 0xAC),
                 textBox1.Text);
             loopc = 30;
+        }
+
+        private void a20_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void a50_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
